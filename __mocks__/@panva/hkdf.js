bbaa3f5df1729ce80d2c0184f41628501d9a4718
master kein <jangkamwoo@gmail.com>
2025-08-20 23:29:21 +0400

    style: format mock files with prettier

--- __mocks__/@panva/hkdf.js
+++ __mocks__/@panva/hkdf.js
@@ -2,5 +2,5 @@ const mockHkdf = jest.fn(async () => new Uint8Array(32));
 module.exports = {
   __esModule: true,
   default: mockHkdf, // default import용
-  hkdf: mockHkdf,    // named import용 (혹시 모를 경우)
+  hkdf: mockHkdf, // named import용 (혹시 모를 경우)
 };
