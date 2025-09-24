58303e8140d008273d0ff7db8cd9cc0b26a474f9
master kein <jangkamwoo@gmail.com>
2025-09-25 00:08:54 +0400

    chore: remove test file after successful migration workflow verification

--- test-js-file.ts
+++
@@ -1,7 +0,0 @@
-// Test JavaScript file to verify pre-commit hook
-const testFunction = () => {
-  console.log('This is a test JS file');
-  return 'Should be blocked by pre-commit hook';
-};
-
-module.exports = testFunction;
