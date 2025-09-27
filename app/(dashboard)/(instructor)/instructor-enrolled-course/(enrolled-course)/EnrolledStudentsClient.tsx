68ff662f844245deb130d75b176a9bda8f355e2a
master kein <jangkamwoo@gmail.com>
2025-09-27 13:26:24 +0400

    fix: use link-primary class for better dark mode visibility of courses management page link

--- app/(dashboard)/(instructor)/instructor-enrolled-course/(enrolled-course)/EnrolledStudentsClient.tsx
+++ app/(dashboard)/(instructor)/instructor-enrolled-course/(enrolled-course)/EnrolledStudentsClient.tsx
@@ -140,7 +140,7 @@ const EnrolledStudentsClient = ({
                                   check the status of your courses in the
                                   <a
                                     href="/instructor-personal-courses"
-                                    className="ms-1 text-primary"
+                                    className="ms-1 link-primary text-decoration-underline"
                                   >
                                     courses management page
                                   </a>
